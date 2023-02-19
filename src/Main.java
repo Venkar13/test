512d69ac6bf4398a0ccf8329e7d1934e2cf3cb4d
Venkar13 <zenaulasena@gmail.com>
2023-02-19 22:44:47 +0300

    push

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("start");
         System.out.println("sout1");
+        System.out.println("sout2");
     }
 }
